0be4724ee8a6e835fde081ce0d5e2e51722503f1
Patryk Osak <pionchaoo@gmail.com>
2022-05-09 23:46:12 +0200

    initialize lectures

--- src/main/java/com/example/demo/Lecture.java
+++ src/main/java/com/example/demo/Lecture.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
 import com.example.demo.entity.AppUser;
+import lombok.Builder;
+import lombok.Data;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.Set;
 
+@Builder
+@Data
 public class Lecture {
     private Long lectureId;
-    private Date startDate;
-    private Date endDate;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
     private Set<AppUser> users;
 }
---
+++ src/main/java/com/example/demo/service/LectureService.java
@@ -0,0 +1,41 @@
+package com.example.demo.service;
+
+import com.example.demo.Lecture;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class LectureService implements CommandLineRunner {
+
+    private List<Lecture> lectures = new ArrayList<>();
+
+    @Override
+    public void run(String... args) throws Exception {
+        lectures.add(Lecture.builder()
+                .lectureId(1L)
+                .startDate(LocalDateTime.of(2021, Month.JUNE, 1, 10, 0))
+                .endDate(LocalDateTime.of(2021, Month.JUNE, 1, 11, 45))
+                .build());
+        lectures.add(Lecture.builder()
+                .lectureId(2L)
+                .startDate(LocalDateTime.of(2021, Month.JUNE, 1, 12, 0))
+                .endDate(LocalDateTime.of(2021, Month.JUNE, 1, 13, 45))
+                .build());
+        lectures.add(Lecture.builder()
+                .lectureId(3L)
+                .startDate(LocalDateTime.of(2021, Month.JUNE, 1, 14, 0))
+                .endDate(LocalDateTime.of(2021, Month.JUNE, 1, 15, 45))
+                .build());
+
+    }
+
+    public List<Lecture> fetchLectureList(){
+        return lectures;
+    }
+}
